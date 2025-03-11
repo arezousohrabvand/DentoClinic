be05c18260d878076bd7a1885d41a5baa7766db9
Arezoo Sohrabvand <arezoosohrabvand@gmail.com>
2025-03-11 18:01:48 -0500

    Fix Python runtime configuration for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "Building the project..."
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+if [ -f /opt/conda/bin/python ]; then
+    /opt/conda/bin/python -m pip install --upgrade pip
+    /opt/conda/bin/pip install -r requirements.txt
+else
+    python3.9 -m pip install --upgrade pip
+    python3.9 -m pip install -r requirements.txt
+fi
 
 echo "Make migrations..."
-python manage.py makemigrations
-python manage.py migrate
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 
 echo "Collect Static..."
-python manage.py collectstatic --noinput --clear 
+python3.9 manage.py collectstatic --noinput --clear 
