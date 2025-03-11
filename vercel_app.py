43257b4b217db20a4581d11936495381db904b81
Arezoo Sohrabvand <arezoosohrabvand@gmail.com>
2025-03-11 18:07:08 -0500

    Update Vercel configuration and fix file paths

--- vercel_app.py
+++ vercel_app.py
@@ -1,2 +1,6 @@
-from dental.wsgi import application
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dental.settings')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 app = application 
